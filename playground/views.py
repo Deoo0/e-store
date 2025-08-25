e975fc8d41a02d1f5acafed8f4b1e76f88e167d3
Lawrence Deo Bersales <lawrencebersales8@gmail.com>
2025-08-25 20:19:13 +0800

    Added Dummy data to database

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,9 @@
+from django.db.models import query
 from django.shortcuts import HttpResponse, render
+from store.models import Product
 
 
-def say_hello(request):
+def say_hello(request): #learning how to retrieve data form db 2:36:58
+    product = Product.objects.get(pk=1)
+        
     return render(request, 'hello.html')
